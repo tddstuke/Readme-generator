2dd9049e2b5980500c525e2baed69be34ea16dee
Todd Stuke <tddstuke@gmail.com>
2022-05-13 17:54:09 -0500

    added questions using inquirer

--- index.js
+++ index.js
@@ -1,13 +1,97 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project? (Required)",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter a title!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of your project (Required)",
+    validate: (descriptionInput) => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log("Please enter a description!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter any installation instructions. If none press Enter.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message:
+      "Enter any current examples of your project in use. If none press Enter",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message:
+      "Enter any guidelines for others to contribute to your project. If none press Enter",
+  },
+  {
+    type: "input",
+    name: "test",
+    message:
+      "Enter any instructions for testing your project. If none press Enter",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Choose a license for your application.",
+    choices: ["MIT", "Apache License v2.0", "GNU General Public License v3.0"],
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Enter your GitHub username (Required).",
+    validate: (userName) => {
+      if (userName) {
+        return true;
+      } else {
+        console.log("Please enter GitHub username!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address (Required)",
+    validate: (email) => {
+      if (email) {
+        return true;
+      } else {
+        console.log("Please enter an email address!");
+        return false;
+      }
+    },
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
